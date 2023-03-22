ebbf4cecd877821615765a934ae391729acaf32a
ozlemezgi <ozlemezgisari@icloud.com>
2023-03-22 18:47:04 +0300

    components are created

---
+++ src/components/Todo/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header';
+import List from './List';
+import Footer from './Footer';
+
+
+function Todo() {
+  return (
+    <div>
+        <Header/>
+        <List/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Todo
